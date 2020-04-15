5ae4553b6182ca66dd27365339ea1343c4db09dc
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-04-15 13:21:58 -0700

    installed nodemon and concurrently packages. Edited the package json to concurrently compile and execute build with start

--- build/index.js
+++ build/index.js
@@ -1,2 +1,6 @@
 "use strict";
 console.log('what is a man');
+var logSomething = function () {
+    console.log('a miserable pile of secrets');
+};
+logSomething();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
 console.log('what is a man');
+
+const logSomething = () => {
+  console.log('a miserable pile of secrets');
+};
+
+logSomething()
