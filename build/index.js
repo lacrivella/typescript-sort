31e23cff7594888f46e072a1130db5eb99689c74
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-04-16 12:06:32 -0700

    initial sorting algor put together

--- build/index.js
+++ build/index.js
@@ -1,6 +1,23 @@
 "use strict";
-console.log('what is a man');
-var logSomething = function () {
-    console.log('a miserable pile of secrets');
-};
-logSomething();
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            // length - i - 1 is for having the greatest number on the right
+            for (var j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    var lefHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = lefHand;
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([10, 4, -12, 80]);
+sorter.sort();
+console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
 class Sorter {
   constructor(public collection: number[]) {}
+
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      // length - i - 1 is for having the greatest number on the right
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const lefHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = lefHand;
+        }
+      }
+    }
+  }
 }
 
 const sorter = new Sorter([10, 4, -12, 80]);
+sorter.sort();
+console.log(sorter.collection);
