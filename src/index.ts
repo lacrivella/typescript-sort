3f8c9754fdf252bd64f74ddfb2989c917698075a
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-05-08 16:08:22 -0700

    npm start in terminal to double check that linkedlist actually works. guess what, it does

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 // const numbersCollection = new NumbersCollection([12, 6, -40, 0]);
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
 // console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection('hjfdkasDSLBLxc');
-const sorter = new Sorter(charactersCollection);
+// const charactersCollection = new CharactersCollection('hjfdkasDSLBLxc');
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(600);
+linkedList.add(-20);
+linkedList.add(42);
+linkedList.add(-8);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollection.data);
+linkedList.print();
